1fc4d43a7d8a64de422b8562596fd3f4ba0d8f27
awbtravels <awbtravelsntours@outlook.com>
2025-06-16 22:54:35 +0200

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,1 @@
-export function LoginPage() {
-  return (
-    <form method="POST" action="/api/auth/login" className="p-4 space-y-4">
-      <input name="email" placeholder="Email" className="border p-2 w-full" />
-      <input name="password" type="password" placeholder="Password" className="border p-2 w-full" />
-      <button className="bg-blue-600 text-white px-4 py-2">Login</button>
-    </form>
-  );
-}
+
