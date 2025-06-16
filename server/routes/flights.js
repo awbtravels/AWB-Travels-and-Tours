49362b6a019a437aea0083878fdb6d1ba90c95fe
awbtravels <awbtravelsntours@outlook.com>
2025-06-17 00:17:28 +0200

    Update flights.js

--- server/routes/flights.js
+++ server/routes/flights.js
@@ -1,8 +1,1 @@
-import express from 'express';
-const router = express.Router();
 
-router.get('/search', (req, res) => {
-  res.json([{ airline: "British Airways", price: "$500" }, { airline: "Delta", price: "$480" }]);
-});
-
-export default router;
