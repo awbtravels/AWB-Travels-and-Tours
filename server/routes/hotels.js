916c2182d88ab4fda3fdb40646be95a0ca459c0b
awbtravels <awbtravelsntours@outlook.com>
2025-06-17 00:17:47 +0200

    Update hotels.js

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,8 +1,1 @@
-import express from 'express';
-const router = express.Router();
 
-router.get('/search', (req, res) => {
-  res.json([{ name: "Hilton London", price: "$120/night" }, { name: "Ibis Hotel", price: "$80/night" }]);
-});
-
-export default router;
